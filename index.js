1ac2f5bb86d88601f55c59ef3d31b4d84071847f
parishben13@gmail.com <parishben13@gmail.com>
2018-04-10 01:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ console.log(`${string}`.toUpperCase())
 function logWhisper(string){
   console.log(`${string}`.toLowerCase())
 }
-function sayHiToGrandma(){
-  return "I can't hear you!"
-}
-function sayHiToGrandma(){
-  return "YES INDEED"
+function sayHiToGrandma(string){
+  if(`${string}` === `${string}`.toLowerCase())
+  return ("I can't hear you!");
+  if(`${string}` === `${string}`.toUpperCase())
+return ("YES INDEED");
+else if(`${string}`==="I love you")
+return("I love you, too.")
 }
   
   
